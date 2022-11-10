c62a8e7a87cff68f45819b89f9d671284dd1aae3
jitunayak <jitunayak715@gmail.com>
2022-11-11 01:24:57 +0530

    secured our endpoint with aws iam

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ module.exports.open = async (event) => {
         statusCode: 200,
         body: JSON.stringify(
             {
-                message: "Go Serverless v3.0! Your function executed successfully!",
+                message: "Go Serverless v3.0! No Auth! Your function executed successfully!",
                 input: event,
             },
             null,
--- src/secure_handler.js
+++ src/secure_handler.js
@@ -1,11 +1,13 @@
 "use strict";
 
 module.exports.secure = async (event) => {
+
+
     return {
         statusCode: 200,
         body: JSON.stringify(
             {
-                message: "Go Serverless v3.0! Your function executed successfully!",
+                message: "Secure msg ",
                 input: event,
             },
             null,
